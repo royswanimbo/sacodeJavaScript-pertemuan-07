18cc5b2c231facc78d8e905615ec861a350cca1b
Roys wanimbo <157364709+royswanimbo@users.noreply.github.com>
2024-06-11 20:05:03 +0900

    tambah file Html,style,dan js

---
+++ script.js
@@ -0,0 +1,12 @@
+// document
+// //getElementById('pesan')
+//     .getElementById("pesan").textContent = "Selamat Datang!";
+
+// document.getElementById(".selamat").textContent = "heppy Coding!";
+
+var getElementById = document.createElement("strong");
+ElemenBaru.textContent = "di SaCode Community!";
+
+document.getElementsById("pesan").appendChild(ElemenBaru);
+
+document.querySelector("selamat").textContent = "Heppy Coding!";
